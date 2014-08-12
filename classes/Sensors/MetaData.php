62079431dab3641a9a7425ecac17ac02e76eeb1b
WPWhiteSecurity <WPWhiteSecurity@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-12 08:25:50 +0000

    Updated version 1.2.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-security-audit-log/trunk@964418 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/Sensors/MetaData.php
+++ classes/Sensors/MetaData.php
@@ -3,16 +3,22 @@
 class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 
 	public function HookEvents() {
-		/*add_action('added_post_meta', array($this,   'EventPostMetaCreated'), 10, 4);
+		add_action('added_post_meta', array($this,   'EventPostMetaCreated'), 10, 4);
 		add_action('update_post_meta', array($this,  'EventPostMetaUpdating'), 10, 3);
 		add_action('updated_post_meta', array($this, 'EventPostMetaUpdated'), 10, 4);
-		add_action('deleted_post_meta', array($this, 'EventPostMetaDeleted'), 10, 4);*/
+		add_action('deleted_post_meta', array($this, 'EventPostMetaDeleted'), 10, 4);
 	}
 	
 	protected $old_meta = array();
 	
+	protected function CanLogPostMeta($meta_id, $object_id, $meta_key){
+		return substr($meta_key, 0, 1) != '_';
+	}
+	
 	public function EventPostMetaCreated($meta_id, $object_id, $meta_key, $_meta_value){
 		$post = get_post($object_id);
+		
+		if(!$this->CanLogPostMeta($meta_id, $object_id, $meta_key))return;
 		
 		switch($post->post_type){
 			case 'page':
@@ -47,16 +53,22 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 	}
 	
 	public function EventPostMetaUpdating($meta_id, $object_id, $meta_key){
-		$this->old_meta[$meta_id] = array($meta_key, get_metadata('post', $object_id, $meta_key, true));
+		static $meta_type = 'post';
+		$this->old_meta[$meta_id] = (object)array(
+			'key' => ($meta = get_metadata_by_mid($meta_type, $meta_id)) ? $meta->meta_key : $meta_key,
+			'val' => get_metadata($meta_type, $object_id, $meta_key, true),
+		);
 	}
 	
 	public function EventPostMetaUpdated($meta_id, $object_id, $meta_key, $_meta_value){
 		$post = get_post($object_id);
 		
+		if(!$this->CanLogPostMeta($meta_id, $object_id, $meta_key))return;
+		
 		if(isset($this->old_meta[$meta_id])){
 			
 			// check change in meta key
-			if($this->old_meta[$meta_id][0] != $meta_key){
+			if($this->old_meta[$meta_id]->key != $meta_key){
 				switch($post->post_type){
 					case 'page':
 						$this->plugin->alerts->Trigger(2064, array(
@@ -64,7 +76,7 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 							'PostTitle' => $post->post_title,
 							'MetaID' => $meta_id,
 							'MetaKeyNew' => $meta_key,
-							'MetaKeyOld' => $this->old_meta[$meta_id][0],
+							'MetaKeyOld' => $this->old_meta[$meta_id]->key,
 							'MetaValue' => $_meta_value,
 						));
 						break;
@@ -74,7 +86,7 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 							'PostTitle' => $post->post_title,
 							'MetaID' => $meta_id,
 							'MetaKeyNew' => $meta_key,
-							'MetaKeyOld' => $this->old_meta[$meta_id][0],
+							'MetaKeyOld' => $this->old_meta[$meta_id]->key,
 							'MetaValue' => $_meta_value,
 						));
 						break;
@@ -85,15 +97,15 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 							'PostType' => $post->post_type,
 							'MetaID' => $meta_id,
 							'MetaKeyNew' => $meta_key,
-							'MetaKeyOld' => $this->old_meta[$meta_id][0],
+							'MetaKeyOld' => $this->old_meta[$meta_id]->key,
 							'MetaValue' => $_meta_value,
 						));
 						break;
 				}
 			}
-			
+			else
 			// check change in meta value
-			if($this->old_meta[$meta_id][1] != $_meta_value){
+			if($this->old_meta[$meta_id]->val != $_meta_value){
 				switch($post->post_type){
 					case 'page':
 						$this->plugin->alerts->Trigger(2060, array(
@@ -102,7 +114,7 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 							'MetaID' => $meta_id,
 							'MetaKey' => $meta_key,
 							'MetaValueNew' => $_meta_value,
-							'MetaValueOld' => $this->old_meta[$meta_id][1],
+							'MetaValueOld' => $this->old_meta[$meta_id]->val,
 						));
 						break;
 					case 'post':
@@ -112,7 +124,7 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 							'MetaID' => $meta_id,
 							'MetaKey' => $meta_key,
 							'MetaValueNew' => $_meta_value,
-							'MetaValueOld' => $this->old_meta[$meta_id][1],
+							'MetaValueOld' => $this->old_meta[$meta_id]->val,
 						));
 						break;
 					default:
@@ -123,7 +135,7 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 							'MetaID' => $meta_id,
 							'MetaKey' => $meta_key,
 							'MetaValueNew' => $_meta_value,
-							'MetaValueOld' => $this->old_meta[$meta_id][1],
+							'MetaValueOld' => $this->old_meta[$meta_id]->val,
 						));
 						break;
 				}
@@ -136,7 +148,11 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor {
 	
 	public function EventPostMetaDeleted($meta_ids, $object_id, $meta_key, $_meta_value){
 		$post = get_post($object_id);
+		
 		foreach($meta_ids as $meta_id){
+			
+			if(!$this->CanLogPostMeta($meta_id, $object_id, $meta_key))continue;
+			
 			switch($post->post_type){
 				case 'page':
 					$this->plugin->alerts->Trigger(2061, array(
